f073aa293bb2c648a6caa291c30ecef08725d5fb
Ashish Patel <Ashish.Patel@rishabh.com>
2016-08-11 10:18:31 +0530

    Ignore file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$oauth2_client_id = 'NTdhYjYyYjk0Y2RmYTAw';
-$oauth2_secret = 'bfac8462e4e4f682f02827bbf35c8a489aeba29f';
-$oauth2_redirect = 'http://a1f98946.ngrok.io/myapp/response.php';
+$oauth2_client_id = 'NTdhYWM2NmI1MWEzNzNi';
+$oauth2_secret = '81c15b1752a8eb32c4103fc928efcd184c81385b';
+$oauth2_redirect = 'http://765e536a.ngrok.io/myapp/response.php';
 $oauth2_access_token = 'pLXtyhOCJygxdXQ4QmdaJgVOINCoNsddp14f0ulV';
-$url_app = 'http://a1f98946.ngrok.io';
+$url_app = 'http://765e536a.ngrok.io';
 ?>
